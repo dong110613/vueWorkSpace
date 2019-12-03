417415a0694dc65b74f4449911f0eaf6c4841504
donghaichen <donghaichenll@163.com>
2019-12-03 15:36:36 +0800

    test

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import axios from 'axios';
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+Vue.prototype.$axios=axios;
+axios.defaults.baseURL = 'http://192.168.199.141:3000';
+// Vue.use(axios);
 new Vue({
   el: '#app',
   router,
